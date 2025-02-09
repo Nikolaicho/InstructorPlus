820c210ed3c18060a5e9928ca8b575797619bba7
Nikolaicho <nikolaistamatov3@abv.bg>
2025-02-09 14:22:35 +0200

    2 new endpoints for deleting classes and getting classes for user not only admins and some minor fixes

--- backend/routers/classes.router.ts
+++ backend/routers/classes.router.ts
@@ -5,8 +5,10 @@ const router = express.Router();
 
 router.get("/getAllAvailableCandidates",authMiddleware.verifyUserCookie,classesController.getCandidates );
 router.post("/signNewClass",authMiddleware.verifyUserCookie,classesController.signNewClass);
-router.post("/getAllClasses",authMiddleware.verifyUserCookie,classesController.getAllClasses)
+router.post("/getAllClasses",authMiddleware.verifyUserCookie,classesController.getAllClassesEndPoint)
 router.post("/searchCandidates",authMiddleware.verifyUserCookie,classesController.searchCandidates)
 router.get("/getTimeLeft",authMiddleware.verifyUserCookie,classesController.timeLeft)
+router.post("/deleteClass",authMiddleware.verifyUserCookie,classesController.deleteClass)
+router.get("/getAllClassesUser",authMiddleware.verifyUserCookie,classesController.getAllClassesUser)
 
 export default router;
