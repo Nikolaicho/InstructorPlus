d457dea42dc27851e002fe50a982b48f5ce27c21
Nikolaicho <nikolaistamatov3@abv.bg>
2025-02-09 14:34:08 +0200

    minor changes

--- backend/routers/superAdmin.router.ts
+++ backend/routers/superAdmin.router.ts
@@ -5,5 +5,5 @@ import authMiddleware from "../middlewares/auth.middleware";
 const router = express.Router();
 
 router.post("/createCorporation",authMiddleware.verifyUserCookie,superAdminController.createCorporation)
-router.post("/createInstructor",authMiddleware.verifyUserCookie,superAdminController.createInstructor)
+
 export default router;
