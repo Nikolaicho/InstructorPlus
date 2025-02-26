8a0332089dbd71bbbd35a9a6c0f6688b0c7e1b43
Nikolaicho <nikolaistamatov3@abv.bg>
2025-02-26 18:41:06 +0200

    new functions to the component

--- frontend/src/components/ReviewRequests.tsx
+++ frontend/src/components/ReviewRequests.tsx
@@ -1,22 +1,43 @@
 import useGetAllRequests from "../hooks/ReviewRequests/useGetAllRequests"
 import useRespondToRequest from "../hooks/ReviewRequests/useRespondToRequest";
+import NavBar from "./NavBar"
+import SideBar from "./SideBar";
+import DoneIcon from '@mui/icons-material/Done';
+import CloseIcon from '@mui/icons-material/Close';
+
 function ReviewRequests(){
-    const {requests} = useGetAllRequests();
+    function removeRequestFromUi(index:number){
+        const temp = [...requests]
+        temp.splice(index,1)
+        setRequests(temp)
+    }
+
+    const {requests,setRequests} = useGetAllRequests();
     const {respondToRequest} = useRespondToRequest()
     return(<>
-    {requests ? requests.map((request:any)=>(
-        <div>
-            <div>{request.firstName} {request.lastName}</div>
-            <button onClick={()=>{
-                respondToRequest(true,request.userId,request.requestId)
-            }}>Приеми</button>
-            <button onClick={()=>{
-                respondToRequest(false,request.userId,request.requestId)
-            }}>Откажи</button>
+        <NavBar/>
+        <div className = "flex">
+            <SideBar/>
+            <div className = "w-full">
+                {requests ? requests.map((request:any,index:number)=>(
+                <div className = "flex flex-row items-center mt-4 ml-4">
+                    <div className = "text-2xl">{request.firstName} {request.lastName}</div>
+                    <div className = "bg-green-500 rounded-lg px-2 border-2 border-black ml-auto mr-4 ">
+                        <div><DoneIcon fontSize="large" onClick = {()=>{
+                            respondToRequest(true,request.userId,request.requestId)
+                            removeRequestFromUi(index)
+                        }} /></div>
+                    </div>
+                    <div className = "bg-red-500 rounded-lg px-2 border-2 border-black mr-4">
+                        <div><CloseIcon fontSize="large" onClick = {()=>{
+                            respondToRequest(false,request.userId,request.requestId)
+                            removeRequestFromUi(index)
+                        }} /></div>
+                    </div>
+                </div>
+            )):<></> }
+         </div>
         </div>
-        
-    )):<></>}
-    <h1>safasfas</h1>
-    </>)
+        </>)
 }
 export default ReviewRequests
